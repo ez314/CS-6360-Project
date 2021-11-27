e70176d5e7df3edf832d26465ac49819eb7065b4
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-11-27 14:22:59 -0600

    Implement sign-up
    Consolidated two sign-up pages into one, with a radio button to handle
    buyers and sellers. Once signed up, users receive their member ID.
    
    Changed queries to use execute() instead, which is much cleaner and more
    powerful for text queries. Be sure to commit after modifying data with
    execute()!
    
    Also fixed logout bug preventing re-login after logout.

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from sqlalchemy import column, text
 
 from website import database
@@ -35,14 +35,70 @@ def login():
 @auth.route('/logout')
 def logout():
     session.clear()
-    return render_template("login.html", info="Successfully logged out")
+    return redirect(url_for('auth.login'))
 
 
-@auth.route('/sign_up_seller')
-def signUpSeller():
-    return render_template("sign_up_seller.html")
+@auth.route('/sign_up')
+def signUp():
+    if request.method == 'GET':
+        if session:
+            # Already logged in
+            return render_template("sign_up_success.html")
+        elif all(x in request.args for x in ['name', 'email', 'password', 'phone', 'address', 'account-type']):
+            # All args passed, create new user in database
+            new_user_vals = 'DEFAULT, "{}", "{}", "{}", "{}", "{}"'.format(
+                request.args.get('name'),
+                request.args.get('email'),
+                request.args.get('password'),
+                request.args.get('phone'),
+                request.args.get('address')
+            )
+            statement = (f'INSERT INTO User (UserID, Name, Email, Passwd, PhoneNum, HomeAddr)\n'
+                         f'    VALUES ({new_user_vals});')
+            database.db.session.execute(text(statement))
 
+            # Fetch ID of inserted user. Must do this because of autoincrement
+            res = database.db.session.execute(text('SELECT LAST_INSERT_ID();')).all()
+            print(res)
+            if not res:
+                return render_template("sign_up.html", info="Error occurred, could not sign up")
+            new_id = res[0][0]
 
-@auth.route('/sign_up_buyer')
-def signUpBuyer():
-    return render_template("sign_up_buyer.html")
+            # Insert user into Buyer or Seller table accordingly
+            if request.args.get('account-type') == 'buyer':
+                new_buyer_vals = '{}, "{}", "{}"'.format(
+                    new_id,
+                    request.args.get('shipping-address'),
+                    request.args.get('city')
+                )
+                statement = (f'INSERT INTO Buyer (BuyerID, Address, City)\n'
+                             f'    VALUES ({new_buyer_vals});')
+                database.db.session.execute(text(statement))
+            elif request.args.get('account-type') == 'seller':
+                new_seller_vals = '{}, "{}", "{}"'.format(
+                    new_id,
+                    request.args.get('account-num'),
+                    request.args.get('routing-num')
+                )
+                statement = (f'INSERT INTO Seller (SellerID, AccountNum, RoutingNum)\n'
+                             f'    VALUES ({new_seller_vals});')
+                database.db.session.execute(text(statement))
+            elif request.args.get('account-type') == 'admin':
+                statement = (f'INSERT INTO Admin (AdminID)\n'
+                             f'    VALUES ({new_id});')
+                database.db.session.execute(text(statement))
+
+            # Set session variables accordingly
+            session['login.id'] = new_id
+            session['login.type'] = request.args.get('account-type')
+            session['login.name'] = request.args.get('name')
+            session['login.password'] = request.args.get('password')
+
+            # Commit changes to database
+            database.db.session.commit()
+
+            return render_template("sign_up_success.html")
+        else:
+            return render_template("sign_up.html")
+    else:
+        return redirect(url_for('auth.signUp'))
