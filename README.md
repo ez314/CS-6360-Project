923674072d412af06dc80d9c029129941c006b6b
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-11-26 23:48:03 -0600

    Add database connection

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # CS-6360-Project
 Online auction site for CS 6360
 
-Steps for executing:
-1.  Install python.
-2.  Install pip and set the path of the pip. 
-3.  Execute : pip install flask. 
-4.  Sync following github repository. 
-5.  Execute main.py
+## How to Run
+1. Install python 3.
+2. Install pip and set the path of the pip. 
+3. Execute: `pip install -r requirements.txt`. 
+4. Download the github repository: `git clone https://github.com/ez314/CS-6360-Project.git`. 
+5. [Create `config.json`](#configuration)
+6. Execute main.py: `python main.py` (or use `python3` if `python` links to Python 2)
+7. 
 
+## Configuration
+Right now, the config file just contains information for connecting to the local MySQL database. 
+
+1. Create a duplicate of `config.example.json` called `config.json`.  
+2. Fill in the configuration values to fit your MySQL setup.
+
+Do not commit the `config.json` file! It contains your password.
--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,24 @@
+import json
+import website.database
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text, column
+
 
 def create_app():
+    config = json.load(open('config.json', 'r'))
+    mysql_server = config['mysql_server']
+    mysql_db = config['mysql_db']
+    mysql_username = config['mysql_username']
+    mysql_password = config['mysql_password']
+
+    sqlalchemy_uri = f'mysql+pymysql://{mysql_username}:{mysql_password}@{mysql_server}/{mysql_db}'
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'onlineAuction_CS6360'
+    app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     from .views import views
     from .auth import auth
@@ -11,6 +27,9 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(prodDetail)
+
+    database.db = SQLAlchemy(app)
+    print('database test:', database.db.session.query(column('BuyerID'), column('Address'), column('City')).from_statement(text('SELECT * FROM ONLINE_AUCTION.Buyer;')).all())
 
     return app
 
