84154738cd4f21c04e4f259b31e787c9faf222a8
mikexie360 <mxie360@gmail.com>
2021-11-30 21:56:22 -0600

    Update home page, new default page
    update home page and new home page.

--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    # Redirect to login if not already logged in
-    if 'login.id' not in session:
-        return redirect(url_for('auth.login'))
+    # # Redirect to login if not already logged in
+    # if 'login.id' not in session:
+    #     return redirect(url_for('auth.login'))
     return render_template("home.html")
