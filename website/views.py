69bd4f5ddb12583533058b1e8e891aba8ca1c66f
AntDevictor <antdevictor@gmail.com>
2021-11-30 21:39:53 -0600

    Fixed issues from merge with main branch changes

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, session, url_for
+from flask import Blueprint, render_template, redirect, session, url_for, request
+from website import database
+from sqlalchemy import column, text
 
 views = Blueprint('views', __name__)
 
@@ -9,3 +11,100 @@ def home():
     if 'login.id' not in session:
         return redirect(url_for('auth.login'))
     return render_template("home.html")
+
+
+@views.route('/adminHome')
+def adminHome():
+    # Get the queries for the tables done
+    userStatement = (f'SELECT * FROM ONLINE_AUCTION.User;')
+    itemStatement = (f'SELECT * FROM ONLINE_AUCTION.Item;')
+
+    userQuery = database.db.session.execute(text(userStatement)).all()
+    itemQuery = database.db.session.execute(text(itemStatement)).all()
+
+    # The tuples for passing the date to html
+    # Turn the queries into tuples
+    userData = tuple(userQuery)
+    itemData = tuple(itemQuery)
+
+    userHeadings = ("ID", "Name", "Email", "Password", "Phone Num", "Home Address", "Is Active")
+    itemHeadings = ("ID", "Name", "Description", "Photo", "Starting Bid", "Bid Increment", "Start Date", "End Date")
+
+    return render_template("adminHome.html", userHeadings=userHeadings, itemHeadings=itemHeadings, userData=userData, itemData=itemData)
+
+@views.route('/adminDeleteUserPage', methods=['GET', 'POST'])
+def adminDeleteUserPage():
+    # Get the queries for the tables done
+    userStatement = (f'SELECT * FROM ONLINE_AUCTION.User;')
+
+    userQuery = database.db.session.execute(text(userStatement)).all()
+
+    # The tuples for passing the date to html
+    # Turn the queries into tuples
+    userData = tuple(userQuery)
+
+    userHeadings = ("ID", "Name", "Description", "Photo", "Starting Bid", "Bid Increment", "Start Date", "End Date")
+
+    if request.method == "POST":
+        try:
+            if request.form['id']:
+                statement = (f'UPDATE ONLINE_AUCTION.User SET IsActive = 0 WHERE UserID={request.form["id"]};')
+                database.db.session.execute(text(statement)).all()
+
+                # Redo query for users to show that user has been successfully deleted
+                userQuery = database.db.session.execute(text(userStatement)).all()
+
+                # The tuples for passing the date to html
+                # Turn the queries into tuples
+                userData = tuple(userQuery)
+
+                return render_template("adminDeleteUserPage.html", userHeadings=userHeadings, userData=userData, info="Successfully Deleted user")
+
+            else:
+                return render_template("adminDeleteUserPage.html", userHeadings=userHeadings, userData=userData)
+
+        except Exception:
+            return render_template("adminDeleteUserPage.html", userHeadings=userHeadings, userData=userData, info="Failed to Delete user")
+
+    # Simply render the page
+    if request.method == "GET":
+        return render_template("adminDeleteUserPage.html", userHeadings=userHeadings, userData=userData, info="No Entered Item")
+
+@views.route('/adminDeleteItemPage', methods=['GET', 'POST'])
+def adminDeleteItemPage():
+    # Get the queries for the tables done
+    itemStatement = (f'SELECT * FROM ONLINE_AUCTION.Item;')
+
+    itemQuery = database.db.session.execute(text(itemStatement)).all()
+
+    # The tuples for passing the date to html
+    # Turn the queries into tuples
+    itemData = tuple(itemQuery)
+
+    itemHeadings = ("ID", "Name", "Description", "Photo", "Starting Bid", "Bid Increment", "Start Date", "End Date")
+
+    if request.method == "POST":
+        try:
+            if request.form['id']:
+                print(request.form["id"])
+                statement = (f'DELETE FROM ONLINE_AUCTION.Item WHERE ItemID = {request.form["id"]};')
+                database.db.session.execute(text(statement)).all()
+
+                # Redo query for items to show that item has been successfully deleted
+                itemQuery = database.db.session.execute(text(itemStatement)).all()
+
+                # The tuples for passing the date to html
+                # Turn the queries into tuples
+                itemData = tuple(itemQuery)
+
+                return render_template("adminDeleteItemPage.html", itemHeadings=itemHeadings, itemData=itemData, info="Successfully Deleted item")
+
+            else:
+                return render_template("adminDeleteItemPage.html", itemHeadings=itemHeadings, itemData=itemData, info="No Entered Item")
+
+        except Exception:
+            return render_template("adminDeleteItemPage.html", itemHeadings=itemHeadings, itemData=itemData, info="Failed to Delete item")
+
+    # Simply render the page
+    if request.method == "GET":
+        return render_template("adminDeleteItemPage.html", itemHeadings=itemHeadings, itemData=itemData)
